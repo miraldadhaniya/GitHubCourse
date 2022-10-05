444bdcfac484b623d3611b5c7c9d903066dcd9b3
miraldadhaniya <miraldadhaniya1988@gmail.com>
2022-10-05 21:55:43 +0100

    first commit

---
+++ src/pages/HomePage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class HomePage {
+
+	public static void main(String arges[]) {
+		System.out.println("Hello Home Page");
+	}
+}
---
+++ src/pages/LoginPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class LoginPage {
+	
+	public static void main(String arges[]) {
+		System.out.println("Hello Login Page");
+	}
+
+}
